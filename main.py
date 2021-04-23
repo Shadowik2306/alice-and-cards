44d1464ab1c2f13c3dfb545f29f2774789a0f9cd
shadowik <zahar.mark@bk.ru>
2021-04-24 02:36:34 +0400

    LETS DO IT

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import logging
 import json
 import os
 from geo import *
+import random
 # импортируем функции из нашего второго файла geo
 
 app = Flask(__name__)
@@ -10,6 +11,9 @@ app = Flask(__name__)
 # Добавляем логирование в файл.
 # Чтобы найти файл, перейдите на pythonwhere в раздел files,
 # он лежит в корневой папке
+
+
+sessionStorage = {}
 
 
 @app.route('/post', methods=['POST'])
@@ -29,24 +33,47 @@ def main():
 
 def handle_dialog(res, req):
     user_id = req['session']['user_id']
+
+    # если пользователь новый, то просим его представиться.
     if req['session']['new']:
-        res['response']['text'] = \
-            'Привет! Я могу показать город или сказать расстояние между городами!'
+        res['response']['text'] = 'Привет! Назови свое имя!'
+        # создаем словарь в который в будущем положим имя пользователя
+        sessionStorage[user_id] = {
+            'first_name': None
+        }
         return
-    # Получаем города из нашего
-    cities = get_cities(req)
-    if not cities:
-        res['response']['text'] = 'Ты не написал название не одного города!'
-    elif len(cities) == 1:
-        res['response']['text'] = 'Этот город в стране - ' + \
-                                  get_geo_info(cities[0], 'country')
-    elif len(cities) == 2:
-        distance = get_distance(get_geo_info(
-            cities[0], 'coordinates'), get_geo_info(cities[1], 'coordinates'))
-        res['response']['text'] = 'Расстояние между этими городами: ' + \
-                                  str(round(distance)) + ' км.'
+
+    # если пользователь не новый, то попадаем сюда.
+    # если поле имени пустое, то это говорит о том,
+    # что пользователь еще не представился.
+    if sessionStorage[user_id]['first_name'] is None:
+        # в последнем его сообщение ищем имя.
+        first_name = get_first_name(req)
+        # если не нашли, то сообщаем пользователю что не расслышали.
+        if first_name is None:
+            res['response']['text'] = \
+                'Не расслышала имя. Повтори, пожалуйста!'
+        # если нашли, то приветствуем пользователя.
+        # И спрашиваем какой город он хочет увидеть.
+        else:
+            sessionStorage[user_id]['first_name'] = first_name
+            res['response']['text'] = \
+                f'Привет, {first_name.title()}! Я могу показать город или сказать расстояние между городами!'
     else:
-        res['response']['text'] = 'Слишком много городов!'
+        # Получаем города из нашего
+        cities = get_cities(req)
+        if not cities:
+            res['response']['text'] = 'Ты не написал название не одного города!'
+        elif len(cities) == 1:
+            res['response']['text'] = 'Этот город в стране - ' + \
+                                      get_geo_info(cities[0], 'country')
+        elif len(cities) == 2:
+            distance = get_distance(get_geo_info(
+                cities[0], 'coordinates'), get_geo_info(cities[1], 'coordinates'))
+            res['response']['text'] = 'Расстояние между этими городами: ' + \
+                                      str(round(distance)) + ' км.'
+        else:
+            res['response']['text'] = 'Слишком много городов!'
 
 
 def get_cities(req):
@@ -56,6 +83,17 @@ def get_cities(req):
             if 'city' in entity['value']:
                 cities.append(entity['value']['city'])
     return cities
+
+
+def get_first_name(req):
+    # перебираем сущности
+    for entity in req['request']['nlu']['entities']:
+        # находим сущность с типом 'YANDEX.FIO'
+        if entity['type'] == 'YANDEX.FIO':
+            # Если есть сущность с ключом 'first_name',
+            # то возвращаем ее значение.
+            # Во всех остальных случаях возвращаем None.
+            return entity['value'].get('first_name', None)
 
 
 if __name__ == '__main__':
